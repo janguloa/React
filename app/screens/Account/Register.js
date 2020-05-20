ffaf63a6e98836ce3feca436b8cf5b8b2a777501
janguloa <jsuesalnugo@gmail.com>
2020-05-19 21:26:33 -0500

    Unidad 7 video 50

--- app/screens/Account/Register.js
+++ app/screens/Account/Register.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { StyleSheet, View, Text, Image } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import RegisterForm from "../../components/Account/RegisterForm"
 
 export default function Register() {
     return (
-        <View>
+        <KeyboardAwareScrollView>
             <Image 
                 source={require("../../../assets/img/5-tenedores.png")}
                 resizeMode="contain"
                 style={styles.logo}
             />
             <View style={styles.viewForm}>
-                <Text>Register Form</Text>
+                <RegisterForm />
             </View>
-        </View>
+        </KeyboardAwareScrollView>
     )
 }
 

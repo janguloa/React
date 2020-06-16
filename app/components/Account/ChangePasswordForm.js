1fd4470201254e093e1e1d3ed00de30fe8365b20
janguloa <jsuesalnugo@gmail.com>
2020-06-16 00:34:14 -0500

    Unidad 8 video 92

--- app/components/Account/ChangePasswordForm.js
+++ app/components/Account/ChangePasswordForm.js
@@ -1,41 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { Button, Input } from "react-native-elements";
 
 export default function ChangePasswordForm(){
+
+    const [showPassword, setShowPassword] = useState(false);
+    const [showPassword1, setShowPassword1] = useState(false);
+    const [showPassword2, setShowPassword2] = useState(false);
+
     return (
         <View style={styles.view}>
             <Input
                 placeholder="Contraseña actual"
                 containerStyle={styles.input}
                 password={true}
-                secureTextEntry={true}
+                secureTextEntry={showPassword ? false : true}
                 rightIcon={{
                     type:"material-community",
-                    name: "eye-outline",
+                    name: showPassword ? "eye-off-outline" : "eye-outline",
                     color: "#c2c2c2",
-                }}
+                    onPress: () => setShowPassword(!showPassword)
+                }
+            }
             />
             <Input 
                 placeholder="Nueva contraseña"
                 containerStyle={styles.input}
                 password={true}
-                secureTextEntry={true}
+                secureTextEntry={showPassword1 ? false: true}
                 rightIcon={{
                     type:"material-community",
-                    name: "eye-outline",
+                    name: showPassword1 ? "eye-off-outline" :"eye-outline",
                     color: "#c2c2c2",
+                    onPress: () => setShowPassword1(!showPassword1)
                 }}
             />
             <Input 
                 placeholder="Repetir nueva contraseña"
                 containerStyle={styles.input}
                 password={true}
-                secureTextEntry={true}
+                secureTextEntry={showPassword2 ? false : true}
                 rightIcon={{
                     type:"material-community",
-                    name: "eye-outline",
+                    name: showPassword2 ? "eye-off-outline" : "eye-outline",
                     color: "#c2c2c2",
+                    onPress: () => setShowPassword2(!showPassword2)
                 }}
             />
             <Button 

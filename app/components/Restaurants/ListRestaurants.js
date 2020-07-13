ab19b3bca25b4b1865081f5144689a2a400736ae
Jesus <jsuesalnugo@gmail.com>
2020-07-12 21:32:54 -0500

    unidad 9 video 119

--- app/components/Restaurants/ListRestaurants.js
+++ app/components/Restaurants/ListRestaurants.js
@@ -37,14 +37,42 @@ export default function ListRestaurants(props) {
 }
 
 function Restaurant(props) {
-    const {restaurant} = props;
+    const { restaurant } = props;
+    const { images, name, description, address } = restaurant.item;
+    const imageRestaurant = images[0];
+
+    const goRestaurant = () => {
+        console.log('OK!!');
+    }
 
     return (
-        <View>
-            <Text>
-                Restaurante 
-            </Text>
-        </View>
+        <TouchableOpacity onPress={goRestaurant}>
+            <View style={styles.viewRestaurant}>
+                <View style={styles.viewRestaurantImage} >
+                    <Image 
+                        resizeMode="cover"
+                        PlaceholderContent={<ActivityIndicator color="fff" />}
+                        source={
+                            imageRestaurant
+                            ? {uri: imageRestaurant}
+                            : {uri: require("../../../assets/img/no_found.png")}
+                        }
+                        style={styles.imageRestaurant}
+                    />
+                </View>
+                <View>
+                    <Text style={styles.restaurantName}>
+                        {name}
+                    </Text>
+                    <Text style={styles.restaurantAddress}>
+                        {address}
+                    </Text>
+                    <Text style={styles.restaurantDescription}>
+                        {description.substr(0,60)}...
+                    </Text>
+                </View>
+            </View>
+        </TouchableOpacity>
     )
 }
 
@@ -53,5 +81,28 @@ const styles = StyleSheet.create({
         marginTop: 10,
         marginBottom: 10,
         alignItems: "center",
+    },
+    viewRestaurant: {
+        flexDirection: "row",
+        margin: 10,
+    },
+    viewRestaurantImage: {
+        marginRight: 15,
+    },
+    imageRestaurant: {
+        width: 80,
+        height: 80,
+    },
+    restaurantName: {
+        fontWeight: "bold"  
+    },
+    restaurantAddress: {
+        paddingTop: 2,
+        color: "grey"
+    },
+    restaurantDescription: {
+        paddingTop: 2,
+        color: "grey",
+        width: 300,
     },
 })

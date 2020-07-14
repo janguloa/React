211fddc811adefb0179af7506d84ef3ff98b5a8f
Jesus <jsuesalnugo@gmail.com>
2020-07-14 14:09:16 -0500

    unidad 10 video 126

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,14 +1,31 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { firebaseApp } from "../../utils/firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import Loading from "../../components/Loading";
+
+const db = firebase.firestore(firebaseApp);
 
 export default function Restaurant(props) {
     const { navigation, route } = props;
     const { id, name } = route.params;
+    const [restaurant, setRestaurant] = useState(null);
 
     navigation.setOptions({ title: name});
 
-    console.log(props);
+    useEffect(() => {
+        db.collection("restaurants")
+            .doc(id)
+            .get()
+            .then((response) => {
+                const data = response.data();
+                data.id = response.id;
+                setRestaurant(data);
+            });
+    }, []);
 
+    if (!restaurant) return <Loading isVisible={true} text="Cargando..." />
 
     return (
         <View>

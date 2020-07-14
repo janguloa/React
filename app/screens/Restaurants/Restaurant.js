cd203ed9600ad7f92fbee4ac195105540fbc31a9
Jesus <jsuesalnugo@gmail.com>
2020-07-14 13:25:39 -0500

    unidad 10 video 125

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 export default function Restaurant(props) {
+    const { navigation, route } = props;
+    const { id, name } = route.params;
+
+    navigation.setOptions({ title: name});
 
     console.log(props);
 

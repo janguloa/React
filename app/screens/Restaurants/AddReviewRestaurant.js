8058fb361d85e742cab463440843c07fc831427f
Jesus <jsuesalnugo@gmail.com>
2020-07-21 23:23:47 -0500

    unidad 12 video 139

--- app/screens/Restaurants/AddReviewRestaurant.js
+++ app/screens/Restaurants/AddReviewRestaurant.js
@@ -1,12 +1,82 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { AirbnbRating, Button, Input } from "react-native-elements";
+import Toast from "react-native-easy-toast";
+import Loading from "../../components/Loading";
+
+import { firebaseApp } from "../../utils/firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const db = firebase.firestore(firebaseApp);
 
 export default function AddReviewRestaurant(props) {
 
     const { navigation, route } = props;
     const { idRestaurant } = route.params;
+    const [rating, setRating] = useState(null);
+    const [title, setTitle] = useState(null);
+    const [review, setReview] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const toastRef = useRef();
 
+    const addReview = () => {
+
+        if(!rating){
+            toastRef.current.show("No has dado ninguna puntuación")
+        }else if(!title){
+            toastRef.current.show("No has ingresado un titulo");
+        }else if(!review){
+            toastRef.current.show("No has ingresado comentarios");
+        }else{
+            setIsLoading(true);
+
+            const user = firebase.auth().currentUser;
+            const payload = {
+                idUser: user.uid,
+                avatarUser: user.photoURL,
+                idRestaurant: idRestaurant,
+                title,
+                review,
+                rating,
+                createAt: new Date(),
+            };
+        
+            db.collection("reviews")
+                .add(payload)
+                .then((response) => {
+                    updateRestaurant();
+                })
+                .catch((error) => {
+                    toastRef.current.show("Error al enviar la review");
+                    setIsLoading(false);
+                });
+        }
+
+    };
+
+    const updateRestaurant = () => {
+        const restaurantRef = db.collection("restaurants").doc(idRestaurant);
+
+        restaurantRef.get().then((response) => {
+            const restaurantData = response.data();
+            const ratingTotal = restaurantData.ratingTotal + rating;
+            const quantityVoting = restaurantData.quantityVoting + 1;
+            const ratingResult = ratingTotal / quantityVoting;
+
+            restaurantRef
+            .update({
+                rating: ratingResult,
+                ratingTotal,
+                quantityVoting
+            })
+            .then(() => {
+                setIsLoading(false);
+                navigation.goBack();
+            });
+        });
+    };
+        
     return (
         <View style={styles.viewBody}>
             <View style={styles.viewRating}>
@@ -14,24 +84,37 @@ export default function AddReviewRestaurant(props) {
                     count={5}
                     reviews={["Pésimo","Deficiente","Normal","Muy Bueno","Excelente"]}
                     size={35}
+                    onFinishRating={(value) => {setRating(value)}}
                 />
             </View>
             <View style={styles.formReview}>
                 <Input 
                     placeholder="Titulo"
                     containerStyle={styles.input}
+                    onChange={(e) => setTitle(e.nativeEvent.text)}
                 />
                 <Input
                     placeholder="Comentario..."
                     multiline={true}
                     inputContainerStyle={styles.textArea}
+                    onChange={(e) => setReview(e.nativeEvent.text)}
                 />
                 <Button 
                     title="Enviar comentarios"
                     containerStyle={styles.btnContainer}
                     buttonStyle={styles.btn}
+                    onPress={addReview}
                 />
             </View>
+            <Toast 
+                ref={toastRef}
+                position="center"
+                opacity={0.9}
+            />
+            <Loading 
+                isVisible={isLoading}
+                text="Enviando comentarios"
+            />
         </View>
     )
 }
--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
 import { map } from "lodash";
-import { Rating, ListItem, Icon } from "react-native-elements";
+import { Rating, ListItem } from "react-native-elements";
+import {useFocusEffect } from "@react-navigation/native";
 import { firebaseApp } from "../../utils/firebase";
 import firebase from "firebase/app";
 import "firebase/firestore";
@@ -21,17 +22,19 @@ export default function Restaurant(props) {
 
     navigation.setOptions({ title: name});
 
-    useEffect(() => {
-        db.collection("restaurants")
-            .doc(id)
-            .get()
-            .then((response) => {
-                const data = response.data();
-                data.id = response.id;
-                setRestaurant(data);
-                setRating(data.rating);
-            });
-    }, []);
+    useFocusEffect(
+        useCallback(() => {
+            db.collection("restaurants")
+                .doc(id)
+                .get()
+                .then((response) => {
+                    const data = response.data();
+                    data.id = response.id;
+                    setRestaurant(data);
+                    setRating(data.rating);
+                });
+        }, [])
+    );
 
     if (!restaurant) return <Loading isVisible={true} text="Cargando..." />
 

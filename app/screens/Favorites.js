a0fdd9668b79b4a011a71ef0c6f25128c4cc3b2f
Jesus <jsuesalnugo@gmail.com>
2020-07-24 00:25:45 -0500

    unidad 13 video 150

--- app/screens/Favorites.js
+++ app/screens/Favorites.js
@@ -10,6 +10,7 @@ import {
 } from "react-native";
 import { Image, Icon, Button } from "react-native-elements";
 import { useFocusEffect } from "@react-navigation/native";
+import Loading from "../components/Loading";
 
 import {firebaseApp} from "../utils/firebase";
 import firebase from "firebase";
@@ -17,12 +18,10 @@ import "firebase/firestore";
 
 const db = firebase.firestore(firebaseApp);
 
-export default function Favorites() {
-
+export default function Favorites(props) {
+    const { navigation } = props;
     const [restaurants, setRestaurants] = useState(null);
     const [userLogged, setUserLogged] = useState(false);
-
-    console.log(restaurants);
 
     firebase.auth().onAuthStateChanged((user) => {
         user ? setUserLogged(true) : setUserLogged(false);
@@ -64,9 +63,91 @@ export default function Favorites() {
         return Promise.all(arrayRestaurants);
     };
 
+    if (!userLogged) {
+        return <UserNoLogged 
+            navigation={navigation}  
+        />
+    }
+
+    if(restaurants?.length === 0) {
+        return <NotFoundREstaurants/>
+    }
+
     return (  
-        <View>
-            <Text>Favorites....</Text>
+        <View style={styles.viewBody}>
+            {restaurants ? (
+                <FlatList 
+                    data={restaurants}
+                    renderItem={(restaurant) => 
+                        <Restaurant restaurant={restaurant} />}
+                    keyExtractor={(item, index) => index.toString()}
+                />
+            ) : (
+                <View style={styles.loaderRestaurants}>
+                    <ActivityIndicator size="large" />
+                    <Text style={{ textAlign: "center"}}>Cargando restaurantes</Text>
+                </View>  
+            )}
         </View>
     );
-};
+}
+
+function NotFoundREstaurants() {
+    return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
+            <Icon
+                type="material-community"
+                name="alert-outline"
+                size={50}
+            />
+            <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+                No tienes restaurantes en tu lista
+            </Text>
+        </View>
+    )
+}
+
+function UserNoLogged(props){
+    const { navigation } = props;
+
+    return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
+            <Icon 
+                type="material-community"
+                name="alert-outline"
+                size={50}
+            />
+            <Text style={{fontSize: 20, fontWeight: "bold", textAlign: "center"}}>
+                Necesitas estar logeado para ver esta sección
+            </Text>
+            <Button 
+                title="Ir al login"
+                containerStyle={{ marginTop: 20, width: "80%"}}
+                buttonStyle={{ backgroundColor: "#00a680"}}
+                onPress={() => navigation.navigate("account", {screen: "login"})}
+            />
+        </View>
+    )
+}
+
+function Restaurant (props) {
+    const { restaurant } = props;
+    const { name } = restaurant.item;
+
+    return(
+        <View>
+            <Text>{name}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    viewBody: {
+        flex: 1,
+        backgroundColor: "#f2f2f2"
+    },
+    loaderRestaurants: {
+        marginTop: 10,
+        marginBottom: 10,
+    },
+})
